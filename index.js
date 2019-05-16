d16fdf4652e094036516181df4482c1e7b8197e8
Patient Padawan <1PatientPadawan@gmail.com>
2019-05-16 11:38:27 -0700

    added parse functionality and minor styling

--- index.js
+++ index.js
@@ -1,2 +1,47 @@
 'use strict';
 
+function displayResults(responseJson) {
+    console.log(responseJson);
+    $('#results-list').empty();
+
+    for (let i = 0; i < responseJson.length; i++) {
+        $('#results-list').append(
+            `<li><h3>${responseJson[i].name}</h3>
+            <a href="${responseJson[i].html_url}">${responseJson[i].html_url}</a></li>`)
+        };
+
+
+    $('#results').removeClass('hidden');
+    
+}
+
+function getGitHubHandle(searchTerm) {
+    const url = `https://api.github.com/users/${searchTerm}/repos`
+
+    console.log(url);
+
+    fetch(url)
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error('it is broken!');
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+        $('#js-error-message').text(`That GitHub handle doesn't exist!`);
+    });
+}
+
+function watchForm() {
+    $('form').submit(event => {
+        event.preventDefault();
+        const searchTerm = $('#js-search-term').val();
+        getGitHubHandle(searchTerm);
+    });
+}
+
+$(function() {
+    console.log('App loaded! Ready to parse!');
+    watchForm();
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+A project involving GET requests from the GitHub API
+Users can search for a particular GitHub handle 
+and then peruse through their available repositories.
